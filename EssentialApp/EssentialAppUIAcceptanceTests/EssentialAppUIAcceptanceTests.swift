975422260bd78f771908266aea76bba99facf41a
Gideon Benz <gideonbenz@gmail.com>
2022-06-04 14:14:56 +0700

    displays remote feed on launch when customer has connectivity

--- EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTests.swift
@@ -8,5 +8,13 @@
 import XCTest
 
 class EssentialAppUIAcceptanceTests: XCTestCase {
-
+    
+    func test_onLaunch_displayRemoteFeedWhenCustomerHasConnectivity() {
+        let app = XCUIApplication()
+        
+        app.launch()
+        
+        XCTAssertEqual(app.cells.count, 22)
+        XCTAssertEqual(app.cells.firstMatch.images.count, 1) //It could be a performance or network issue. That's why UI tests are so fragile - they can suddenly start failing unexpectedly, and it's hard to tell why.
+    }
 }

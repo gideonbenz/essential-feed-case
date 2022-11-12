1680290a1c78f6c3d2a074975e76841fb35f3c00
Gideon Benz <gideonbenz@gmail.com>
2022-11-12 18:19:32 +0700

    duplicate RemoteFeedLoader as RemoteLoader

---
+++ EssentialFeed/EssentialFeed/SharedAPI/RemoteLoader.swift
@@ -0,0 +1,46 @@
+//
+//  RemoteLoader.swift
+//  EssentialFeed
+//
+//  Created by Gideon Benz on 12/11/22.
+//
+
+public final class RemoteLoader: FeedLoader {
+    private  let url: URL
+    private let client: HTTPClient
+    
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+    
+    public typealias Result = FeedLoader.Result
+    
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+    
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
+            switch result {
+            case let .success((data, response)):
+                 completion(RemoteLoader.map(data, response: response))
+                
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+    
+    private static func map(_ data: Data, response: HTTPURLResponse) -> Result {
+        do {
+            let items = try FeedItemsMapper.map(data, from: response)
+            return .success(items)
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}

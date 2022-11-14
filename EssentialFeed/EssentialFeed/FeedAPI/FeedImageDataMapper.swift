983add63165084d328610bad2aba2c2b7e2071a0
Gideon Benz <gideonbenz@gmail.com>
2022-11-14 13:09:42 +0700

    test FeedImageDataMapper in isolation

---
+++ EssentialFeed/EssentialFeed/FeedAPI/FeedImageDataMapper.swift
@@ -0,0 +1,20 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeed
+//
+//  Created by Gideon Benz on 14/11/22.
+//
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+              throw Error.invalidData
+        }
+        
+        return data
+    }
+}
